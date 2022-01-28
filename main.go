27ed335c92d251c4a76f12c181b8068ee8dca764
Ramzi Chikh Bled <gr_chikhbled@esi.dz>
2022-01-28 16:39:50 +0100

    added mutex locks for db read and writes

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
 
 var db map[string]string
+var m sync.RWMutex
 
 func main() {
 	db = make(map[string]string)
-
 	router := gin.Default()
 	router.GET("/:key", getData)
 	router.GET("/", getAll)
@@ -20,24 +21,40 @@ func main() {
 }
 
 func getAll(c *gin.Context) {
+	m.RLock()
 	c.JSON(http.StatusOK, db)
+	m.RUnlock()
 }
 
 func getData(c *gin.Context) {
+	m.RLock()
 	data, ok := db[c.Param("key")]
 	if ok {
 		c.JSON(http.StatusOK, gin.H{c.Param("key"): data})
 	}
+	m.RUnlock()
 }
 
 func putData(c *gin.Context) {
+	m.Lock()
 	db[c.Param("key")] = c.Param("value")
-}
-
-func deleteData(c *gin.Context) {
+	m.Unlock()
+	m.RLock()
 	data, ok := db[c.Param("key")]
 	if ok {
 		c.JSON(http.StatusOK, gin.H{c.Param("key"): data})
+	}
+	m.RUnlock()
+}
+
+func deleteData(c *gin.Context) {
+	m.RLock()
+	data, ok := db[c.Param("key")]
+	m.RUnlock()
+	if ok {
+		m.Lock()
 		delete(db, c.Param("key"))
+		m.Unlock()
+		c.JSON(http.StatusOK, gin.H{c.Param("key"): data})
 	}
 }
